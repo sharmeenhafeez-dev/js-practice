f809b82ca0d9b1c3052f94f4f19f35b539493899
sharmeenhafeez-dev <sharmeenhafeez21@gmail.com>
2023-04-05 16:50:47 +0500

    FEAT:js practice work

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// day 1 ; 5 april 2023
+
+
+
+alert ( "hi" );
+prompt ("your email","abc@gmail.com")
+alert("user")
+
+
+var ace =["welcome","hi"]
+
+document.write("ace")
+
+console.log("ace");
+var name = "Mark";
