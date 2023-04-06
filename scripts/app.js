90a9bf340facc2ddc915de0c449470e0aadc2904
sharmeenhafeez-dev <sharmeenhafeez21@gmail.com>
2023-04-06 22:37:35 +0500

    FEAT js more practice work

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,69 @@
-// day 1 ; 5 april 2023
+// // day 1 ; 5 april 2023
 
 
 
-alert ( "hi" );
-prompt ("your email","abc@gmail.com")
-alert("user")
+// alert ( "hi" );
+// prompt ("your email","abc@gmail.com")
+// alert("user")
 
 
-var ace =["welcome","hi"]
+// var ace =["welcome","hi"]
 
-document.write("ace")
+// document.write("ace")
 
-console.log("ace");
-var name = "Mark";
+// console.log("ace");
+// var name = "Mark";
+
+
+
+// var popularNumber = 12 - 24;
+
+// console.log("popularNumbe");
+
+
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+//  alert  ("defaultAnswer")
+//  console.log("alert ")
+
+
+console.log(12+35)
+console.log(7-5)
+console.log(6*5)
+console.log(32/5)
+console.log(5**2)  
+
+
+
+
+
+
+// alert('Hello World')
+// var userPhone =+prompt("Enter Your Phone Number","03212000000")
+// var user = "o3210000000"
+// var userAge = 20
+// console.log("Hello " + userAge+" " +user)
+
+
+
+// var dany raj
+// var danyRaj camelCase  Var ka sec word ka letter uppercase my hoga
+// DanyRaj PascalCase Var ka first word or sec word ka letter uppercase my hoga
+// sany_Raj snake_case  Var ka first word k or sec word k darmiyn my (_) ya likhna h hoga 
+
+
+
+
+
+//  var incomeUser1 =+ prompt("income user 1")
+// var incomeUser2 =+ prompt("income user 2")
+// alert(incomeUser1+incomeUser2)
+// alert((incomeUser1) +(incomeUser2))
+// console.log("incomeUser1" + "incomeUser2")
+
+
+
+
+
